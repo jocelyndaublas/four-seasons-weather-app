766c48cf38bc42c2cea6dc52ac6870af3e03da91
jocelyndaublas <jocelyndaublas@gmail.com>
2021-09-04 00:17:22 -0600

    JS forecast completed

--- index.js
+++ index.js
@@ -41,32 +41,39 @@ function formatTime() {
 }
 formatTime();
 //forecast
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+  return days[day];
+}
 function displayForecast(response){
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
+
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu","Fri","Sat"];
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay, index) {
+    if (index<6){
   forecastHTML = forecastHTML + `
-  <div class="col-2>
-  <div>  
-<div id="forecast" class="dateForecast">${day}</div> 
-<img src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" width="46
+  <div class="col-2">
+<div class="dateForecast">${formatDay(forecastDay.dt)}</div> 
+<img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="icon" width="46
 "> 
-<span class="tempForecast">18° </span>
-</div>
+<span class="tempForecast">${Math.round(forecastDay.temp.max)}° </span>
 </div> `;
+    }
 });
 forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates){
+  console.log(coordinates);
   let apiKey = "ca5da085c3334fa2974d520a9a4b8c12";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayForecast);
 }
 //search engine
 function currentWeather(response) {
-  console.log(response);
   document.querySelector("#current-city").innerHTML = response.data.name;
   document.querySelector("#current-temperature").innerHTML = Math.round(
     response.data.main.temp
